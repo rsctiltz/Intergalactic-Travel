87a04c18a1cd0ccaba6f7330830b44cbf41c4dac
rsctiltz <rsctiltz@gmail.com>
2014-05-13 10:24:43 -0500

    Initial Commit

---
+++ Intergalactic Travel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Intergalactic Travel
+//
+//  Created by Ryan Tiltz on 5/13/14.
+//  Copyright (c) 2014 Ryan Tiltz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
